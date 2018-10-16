101fa2b75cb7c2e639af5c4be5dc93a39c6b71cf
hdevelly <hugo.develly@etu.univ-lyon1.fr>
2018-10-16 16:57:32 +0200

    intégration quizz + bannière

--- src/script/script.js
+++ src/script/script.js
@@ -1,16 +1,17 @@
 console.log('coucou');
 window.onload = function(){
     console.log('yolo');
-    var selectedCategory;
-    var selectElement = document.getElementById('nav-categories').options[document.getElementById('nav-categories').selectedIndex].text;
     var wrapper = document.getElementById('global-wrapper');
+    /*var selectedCategory;
+    var selectElement = document.getElementById('nav-categories').options[document.getElementById('nav-categories').selectedIndex].text;
+    
 
     function getSelectedElement(){
         var selectedCategory = selectElement.options[document.getElementById('nav-categories').selectedIndex].text;
     }
 
     //selectElement.addEventListener('click', getSelectedElement);
-
+    */
     // Chargement du module
     var xhr= new XMLHttpRequest();
     xhr.open('GET', 'pages/quizz.html', true);
