5330dcb38652e1947e2aeba0b5f76bd0546c75f9
S.AITZIANE <sofian.aitziane@gmail.com>
2018-10-17 23:32:51 +0200

    Ajout d'un simple formulaire de connexion pour tests avec le serveur
    #server.js
    Prise en charge de la connexion d'un utilisateur

--- server.js
+++ server.js
@@ -2,30 +2,38 @@ const http = require("http");
 const fs = require('fs'); //filesystem
 const url = require('url');
 
-const users = [
-    {
-        username: 'toto',
-        password: 'toto',
-        score: 0
-    },
-    {
-        username: 'tata',
-        password: 'tata',
-        score: 0
-    }
-];
-
 const server = http.createServer();
 server.listen(8080);
 server.on('request', (requete, reponse) => {
-    console.log(requete.url)
+    var users = [
+        {
+            "username": "toto",
+            "password": "toto",
+            "score": 0
+        },
+        {
+            "username": "tata",
+            "password": "tata",
+            "score": 0
+        }
+    ];
+    console.log(users[0]["username"]);
     function readStream(chemin){
         const readStream = fs.createReadStream(__dirname+chemin); //renvoie un stream
         readStream.on('data', data => reponse.write(data)); // on lit le fichier tant qu'il y a des données à lire
         readStream.on('end', () => reponse.end());
     }
-    /*const parse = url.parse(requete.url, true); // parse l'url et en fait un JSON;
-    parse.query;*/
+    for(var i=0;i<users.length;i++){
+        const parse = url.parse(requete.url, true);
+        parse.query;
+        if(parse.query["username"] === users[i]["username"] && parse.query["password"] === users[i]["password"]){
+            console.log("okk let's go");
+            break;
+        }
+        else{
+            console.log("try again");
+        }
+    }
     switch(requete.url) { // on récupère l'url
         case '/':
             readStream('/index.html');
@@ -35,7 +43,10 @@ server.on('request', (requete, reponse) => {
         break;
         case '/pages/connection.html':
             readStream('/pages/connection.html');
-        break;            
+        break;
+        case '/pages/leaderboard.html':
+            readStream('/pages/leaderboard.html');
+        break;          
         default:
             if (fs.existsSync(__dirname + requete.url)) {
                 readStream(requete.url);
