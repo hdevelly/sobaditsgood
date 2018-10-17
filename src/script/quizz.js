e5c38b801f29b6b0cb41b843edd08839ec7a07ad
Hugo <hugo.develly@etu.univ-lyon1.fr>
2018-10-17 20:10:47 +0200

    son et image
    Refonte architecture sons
    Fonctions son + image

--- src/script/quizz.js
+++ src/script/quizz.js
@@ -5,47 +5,41 @@ const tubes = [
         titre: "Ohio",
         auteur: "Isabelle Adjani",
         categorie: 80,
-        songPath: "lwrAdjOhi",
-        imgPath: "80AO",
+        path: "80AO",
         valeur: 3
     },
     {
         titre: "Take On Me",
         auteur: "a-ha",
         categorie: 80,
-        songPath: "uprAhaTak",
-        imgPath: "80AT",
+        path: "80AT",
         valeur: 3
     },
     {
         titre: "Maniac",
         auteur: "Michael Sembello",
         categorie: 80,
-        songPath: "uprSemMan",
-        imgPath: "80SM",
+        path: "80SM",
         valeur: 3
     },
     {
         titre: "Boule de Flipper",
         auteur: "Corynne Charby",
         categorie: 80,
-        songPath: "lwrChaBou",
-        imgPath: "80CB",
+        path: "80CB",
         valeur: 3
     },
     {
         titre: "Oops!... I Did It Again",
         auteur: "Britney Spears",
         categorie: 90,
-        songPath: "lwrSpeOop",
-        imgPath: "90SO",
+        path: "90SO",
         valeur: 3
     },
     {
         titre: "Mon papa à moi",
         auteur: "Stomy Bugsy",
-        songPath: "uprBugMon",
-        imgPath: "90BM",
+        path: "90BM",
         categorie: 90,
         valeur: 3
     },
@@ -53,49 +47,42 @@ const tubes = [
         titre: "Partir un jour",
         auteur: "2be3",
         categorie: 90,
-        songPath: "upr2bePar",
-        imgPath: "902P",
+        path: "902P",
         valeur: 3
     },
     {
         titre: "Pour que tu m'aimes encore",
         auteur: "Céline Dion",
         categorie: 90,
-        songPath: "lwrDioPou",
-        imgPath: "90DP",
+        path: "90DP",
         valeur: 3
     },
     {
         titre: "Dragostea Din Tei",
         auteur: "O-Zone",
         categorie: 2000,
-        songPath: "uprOzoDra",
-        imgPath: "2000OD",
+        path: "2000OD",
         valeur: 3
     },
     {
         titre: "Les Sardines",
         auteur: "Patrick Sébastien",
         categorie: 2000,
-        songPath: "uprSebLes",
-        imgPath: "2000SL",
+        path: "2000SL",
         valeur: 3
     },
     {
         titre: "Lady Marmelade",
         auteur: ["Christina Aguilera", "Lil'Kim", "Mya", "Pink"],
         categorie: 2000,
-        songPath: "lwrMouLad",
-        imgPath: "2000ML",
+        path: "2000ML",
         valeur: 3
     },
     {
         titre: "I Kissed a Girl",
         auteur: "Katy Perry",
         categorie: 2000,
-        songPath: "lwrPerIki",
-        imgPath:'lwrPerIki',
-        imgPath: "2000PI",
+        path: "2000PI",
         valeur: 3
     }
 ];
@@ -104,10 +91,23 @@ console.log("Je suis dans le quizz");
 
 window.onload = function(){
 
+    function stopTune(event){
+        let audio = this.nextSibling;
+        audio.pause();
+    }
+
+    function playTune(event){
+        let audio = this.nextSibling;
+        audio.currentTime = Math.random()*audio.duration;
+        audio.play();
+    }
+
     function buildVignettes(array){
         let vignettes = "";
         array.forEach(function(element){
-            vignettes += "<div class='song'><img class='thumbnail secret' src='../src/img/" + element.imgPath + ".png'/><input type='text' class='answer'/></div>";
+            vignettes += "<div class='song'><img class='thumbnail secret' src='../src/img/" + element.path + ".png'/>";
+            vignettes += "<audio src='../src/sound/" + element.path + ".mp3'></audio>";
+            vignettes += "<input type='text' class='answer'/></div>";
         });
         return vignettes;
     }
@@ -121,15 +121,14 @@ window.onload = function(){
     }
 
     function loadSongs(){
-        let songsArray = JSON.stringify(tubes);
+        let songsArray = JSON.parse(JSON.stringify(tubes));
         let htmlNode = "";
         if(selectedCategory != 1){
-            songsArray = filterArray(JSON.parse(songsArray), {categorie : selectedCategory});
+            songsArray = filterArray(songsArray, {categorie : selectedCategory});
         }
         
         htmlNode =  buildVignettes(songsArray);
-
-        console.log(htmlNode);
+        document.getElementById('wrapper-quizz').innerHTML = htmlNode;
 
     }
 
@@ -139,8 +138,15 @@ window.onload = function(){
     
     //Écouteurs
     document.getElementById('categories').addEventListener('change', function(){
+        document.getElementById('validate-quizz').classList.remove('hidden');
         getSelectedElement();
         loadSongs();
+        let nodes = document.querySelectorAll('.thumbnail');
+        nodes.forEach(function(node){
+            node.addEventListener('mouseenter', playTune);
+            node.addEventListener('mouseleave', stopTune);
+        });
     });
+
 
 };
