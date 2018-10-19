0981946ebdda50e3acbb823744ece71d4ffb3192
SAZ-HP\SAZ <sofian.ait-ziane@groupekpf.fr>
2018-10-19 14:46:43 +0200

    ajout d'user pris en compte

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const http = require("http");
 const fs = require('fs'); //filesystem
 const url = require('url');
+const { parse } = require('querystring');
+const { Server } = require('ws');
 
 const server = http.createServer();
 server.listen(8080);
@@ -17,13 +19,12 @@ server.on('request', (requete, reponse) => {
             "score": 0
         }
     ];
-    console.log(users[0]["username"]);
     function readStream(chemin){
         const readStream = fs.createReadStream(__dirname+chemin); //renvoie un stream
         readStream.on('data', data => reponse.write(data)); // on lit le fichier tant qu'il y a des données à lire
         readStream.on('end', () => reponse.end());
     }
-    for(var i=0;i<users.length;i++){
+    /*for(var i=0;i<users.length;i++){
         const parse = url.parse(requete.url, true);
         parse.query;
         if(parse.query["username"] === users[i]["username"] && parse.query["password"] === users[i]["password"]){
@@ -33,7 +34,7 @@ server.on('request', (requete, reponse) => {
         else{
             console.log("try again");
         }
-    }
+    }*/
     switch(requete.url) { // on récupère l'url
         case '/':
             readStream('/index.html');
@@ -43,6 +44,17 @@ server.on('request', (requete, reponse) => {
         break;
         case '/pages/connection.html':
             readStream('/pages/connection.html');
+            if (requete.method === 'POST') {
+                var body='';
+                requete.on('data', form =>{
+                    body+=form.toString();
+                });
+                requete.on('end', () =>{
+                    users.push({username:parse(body)["username"], password:parse(body)["password"], score:0});
+                    //console.log(parse(body)["username"]);
+                    console.log(users);
+                });
+            }
         break;
         case '/pages/leaderboard.html':
             readStream('/pages/leaderboard.html');
